e52605d6e7c51549b73448cd60d18cdb62d99d09
rasathelamedude <rasyar.safen19@gmail.com>
2026-06-20 11:59:31 +0300

    feat: setup zustand store for global user state management

---
+++ src/store/userStore.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+import type { User } from "../types/auth";
+
+interface UserStore {
+  user: User | null;
+  isAuthLoading: boolean;
+  setUser: (user: User) => void;
+  setIsAuthLoading: (isAuthLoading: boolean) => void;
+}
+
+export const useUserStore = create<UserStore>((set) => ({
+  user: null,
+  isAuthLoading: false,
+
+  setUser: (user: User) => set({ user }),
+  setIsAuthLoading: (isAuthLoading: boolean) => set({ isAuthLoading }),
+}));
