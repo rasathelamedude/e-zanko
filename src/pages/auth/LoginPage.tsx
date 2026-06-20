8ea0c0465fb01d6684dd126ab17a3a0ca4d7215a
ShyaAyad <shyaayad1@gmail.com>
2026-06-20 12:26:05 +0300

    fix: move LoginPage into pages/auth folder

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,6 +1,6 @@
-import { Button } from "../components/ui/button"; 
-import { Label } from "../components/ui/label";
-import { Input } from "../components/ui/input";
+import { Button } from "../../components/ui/button"; 
+import { Label } from "../../components/ui/label";
+import { Input } from "../../components/ui/input";
 import { useState } from "react";
 import logo from "../assets/images/kurdistan-region-goverment-logo.png";
 
