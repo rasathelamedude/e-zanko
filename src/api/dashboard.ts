c3d41f32cad25480f9d9e1c7c1e8db04586a764b
rasathelamedude <rasyar.safen19@gmail.com>
2026-06-21 15:43:39 +0300

    chore: write mock API calls to fetch analytics

---
+++ src/api/dashboard.ts
@@ -0,0 +1,9 @@
+export const getDashboardAnalytics = async () => {
+  // TODO: call GET /api/dashboard endpoint once it's implemented
+  
+  return {
+    totalUniversities: 32,
+    totalPendingLetters: 18,
+    totalApprovedLettersThisMonth: 14,
+  };
+};
