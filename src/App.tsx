5697902276253d4c36ea9bbdb706e6e4b60b2b8b
rasathelamedude <rasyar.safen19@gmail.com>
2026-06-22 10:35:33 +0300

    feat: define application layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
+import AppLayout from "./layouts/AppLayout";
 import LoginPage from "./pages/auth/LoginPage";
 import DashboardPage from "./pages/dashboard"; // Automatically imports index.tsx
 import FacultiesPage from "./pages/universities/FacultiesPage";
 import UniversitiesPage from "./pages/universities/UniversitiesPage";
 import { Route, Routes } from "react-router-dom";
-import Sidebar from "./components/layout/Sidebar";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<DashboardPage />} />
-      <Route path="/universities" element={<UniversitiesPage />} />
-      <Route
-        path="/universities/:universityId/faculties"
-        element={<FacultiesPage />}
-      />
+
+      {/* Routes under AppLayout have the same structure (sidebar + header) */}
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/universities" element={<UniversitiesPage />} />
+        <Route
+          path="/universities/:universityId/faculties"
+          element={<FacultiesPage />}
+        />
+      </Route>
     </Routes>
   );
 }
