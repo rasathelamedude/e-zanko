b9847f007249adbb14b0c9fd948e82fde5a50005
rasathelamedude <rasyar.safen19@gmail.com>
2026-06-24 17:12:00 +0300

    chore: wire up protected routes inside App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,44 @@ import FacultiesPage from "./pages/universities/FacultiesPage";
 import UniversitiesPage from "./pages/universities/UniversitiesPage";
 import ReportsPage from "./pages/reports";
 import { Route, Routes } from "react-router-dom";
+import ProtectedRoutes from "./components/common/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
 
-      {/* Routes under AppLayout have the same structure (sidebar + header) */}
-      <Route element={<AppLayout />}>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/universities" element={<UniversitiesPage />} />
-        <Route
-          path="/universities/:universityId/faculties"
-          element={<FacultiesPage />}
-        />
-        <Route
-          path="/universities/:universityId/faculties/:facultyId/departments"
-          element={<DepartmentsPage />}
-        />
-        <Route
-          path="/universities/:universityId/faculties/:facultyId/departments/:departmentId/courses"
-          element={<CoursesPage />}
-        />
-        <Route path="/letters" element={<LettersPage />} />
-        <Route path="/reports" element={<ReportsPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
+      {/* Routes under ProtectedRoutes require logged in users */}
+      <Route element={<ProtectedRoutes />}>
+        {/* Routes under AppLayout have the same structure (sidebar + header) */}
+        <Route element={<AppLayout />}>
+          {/* Polymorphic Routes */}
+          <Route path="/" element={<DashboardPage />} />
+
+          {/* Shared Routes */}
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/letters" element={<LettersPage />} />
+          <Route path="/reports" element={<ReportsPage />} />
+
+          {/* Dedicated Routes (MINISTRY_ADMIN) */}
+          <Route
+            element={<ProtectedRoutes allowedRoles={["MINISTRY_ADMIN"]} />}
+          >
+            <Route path="/universities" element={<UniversitiesPage />} />
+            <Route
+              path="/universities/:universityId/faculties"
+              element={<FacultiesPage />}
+            />
+            <Route
+              path="/universities/:universityId/faculties/:facultyId/departments"
+              element={<DepartmentsPage />}
+            />
+            <Route
+              path="/universities/:universityId/faculties/:facultyId/departments/:departmentId/courses"
+              element={<CoursesPage />}
+            />
+          </Route>
+        </Route>
       </Route>
     </Routes>
   );
