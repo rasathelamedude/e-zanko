5c328e4b124c20b687db737929f79e5be44fc712
__rasyar <rasyar.safen19@gmail.com>
2026-06-28 08:48:41 +0300

    Feat/dean UI (#46)
    * feat: add dashboard page for the dean and test the dean's ui
    
    * feat: add dean's reports page

--- src/pages/reports/index.tsx
+++ src/pages/reports/index.tsx
@@ -1,12 +1,14 @@
 import MinistryReportsPage from "./components/MinistryReportsPage";
 import UniversityReportPage from "./components/UniversityReportPage";
 import { useUserStore } from "../../store/userStore";
+import DeanReportsPage from "./components/DeanReportsPage";
 
 const ReportsPage = () => {
   const { user } = useUserStore();
 
   if (user?.role === "MINISTRY_ADMIN") return <MinistryReportsPage />;
   if (user?.role === "UNIVERSITY_ADMIN") return <UniversityReportPage />;
+  if (user?.role === "DEAN") return <DeanReportsPage />;
 };
 
 export default ReportsPage;
