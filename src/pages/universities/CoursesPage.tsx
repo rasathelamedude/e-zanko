39c0fba2a64ac2c37979bd288299ce51ea6b4975
Rivan Main <rivan.farok@gmail.com>
2026-07-02 09:19:20 +0300

    chore/EZK-55-define-kurdish-terms

--- src/pages/universities/CoursesPage.tsx
+++ src/pages/universities/CoursesPage.tsx
@@ -22,8 +22,6 @@ import { Label } from "../../components/ui/label";
 //   ComboboxList,
 // } from "../../components/ui/combobox";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
-import { mockFaculties } from "./FacultiesPage";
-import { mockDepartments } from "./DepartmentsPage";
 import { useBreadcrumbAccess } from "../../hooks/useBreadcrumbAccess";
 import { BreadcrumbItem } from "../../components/common/BreadcrumbItem";
 import type { UserScope } from "../../types/auth";
